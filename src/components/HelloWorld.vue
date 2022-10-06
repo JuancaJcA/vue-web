ae64059c3b44a0a5c4e0a55cd2c9efc7ac005ae8
AleFlores15 <102682405+AleFlores15@users.noreply.github.com>
2022-10-06 11:58:33 -0400

    Update HelloWorld.vue
    hello world changed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -56,38 +56,9 @@
         <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
       </li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+       <h3>Docente: Rolando Marcelo Ticona Barroso</h3> <br>
+    <h3>Fecha de Entrega: 06/10/2022 </h3> <br>
+    <h3>Semestre 2-2022 </h3> <br>
   </div>
 </template>
 
