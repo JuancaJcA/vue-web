908779bd5333ad46635c6349ff6849386953a7ff
DiegoCAlvaradoC <113271890+DiegoCAlvaradoC@users.noreply.github.com>
2022-10-06 11:47:34 -0400

    participantes1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>
+      Participantes:
+    </h2>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
+      <a href="https://github.com/DiegoCAlvaradoC" target="_blank" rel="noopener"
+        >Diego Alvarado Callisaya</a
       >.
     </p>
     <h3>Diegos</h3>
